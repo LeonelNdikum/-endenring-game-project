f32b22f722aae5b14b748f1a16bfbc0ac9401353
LeonelNdikum <ndikumleonelkubong@gmail.com>
2026-01-17 11:00:03 +0000

    Improved game conditions
    Refactor class selection and battle logic for clarity and redundancy removal.

--- eldernring.sh
+++ eldernring.sh
@@ -1,35 +1,112 @@
 #!/bin/bash
+echo "Welcome Leonel, please sellect your starting class"
 
-echo "you die"
+echo "1- samurai"
+echo "2- prisoner"
+echo "3- Prophet"
 
-# First Beast battle
+echo "Please enter class number from 1-3"
 
-beast=$(($RANDOM % 2))
+read class
 
-echo "your first beast approaches.Prepare to battle.Pick a number between 0-1. (0/1)"
+if [[ $class==1 ]]; then
 
-read  tarnished
+                type="samurai"
+                hp=10
+                attack=20
 
-if [[ $beast == $tarnished ]]; then
-	echo"Beast vanquished!! congrats fellow tarnished"
-	if [[ $user == "root" ]]; then
-	echo "beast vanished"
-else 
-      echo "you die"
-	exit 1
-fi 
+elif [[ $class==2 ]]; then
+                 type="prisoner"
+                 hp=20
+                 attack=4
+
+elif [[ $class==3 ]]; then
+
+                 type="prophet"
+                 hp =4
+                  attack=4
+                  else
+
+echo "invalid option"
+
+ exit 2
+
+fi
+echo "you have choosen class $type ,your hp is $hp and your attack is $attack"
+
+# Prepare for your first battle
+
+#!/bin/bash
+
+echo "Welcome Leonel, please sellect your starting class"
+
+echo "1- samurai"
+echo "2- prisoner"
+echo "3- Prophet"
+
+echo "Please enter class number from 1-3"
+
+read class
+
+if [[ $class==1 ]]; then
+
+                type="samurai"
+                hp=10
+                attack=20
+
+elif [[ $class==2 ]]; then
+                 type="prisoner"
+                 hp=20
+                 attack=4
+
+elif [[ $class==3 ]]; then
+
+                 type="prophet"
+                                hp =4
+                  attack=4
+
+else
+
+echo "invalid option"
+
+ exit 2
+
+fi
+echo "you have choosen class $type ,your hp is $hp and your attack is $attack"
+# Prepare for your first battle
+
+Beast=$(( $RANDOM % 2 ))
+
+echo " pick a number between  0 and 1 (0/1)"
+
+    read leonel
+    
+if [[ $Beast == $leonel && 47 -gt 23 ]]; then
+
+        echo " Beast killed!!.Congrats leonel"
+
+else
+    echo " You die"
+        exit 1
+fi
 
 sleep 2
 
-echo "Boss battle, Get scared.It's Margit.Pick a number between 0-9. (0-9)"
+echo " Boss battle.get scared,its Margrit.Pick a number between 0-9 (0/9)"
 
-read tarnished
+         read leonel
 
-beast=$(($RANDOM % 10))
+Beast=$(( $RANDOM % 10 ))
 
-if [[ $beast == $tarnished ||$tarnished == "coffee" ]]; then
-    echo " Beast vanquished!! congrats leonel"
-else
-    echo "you die"
-exit 2
+if [[ $Beast == $leonel ||$leonel == "coffee" ]]; then
+
+        echo "Beast Vanquished!! congrats leonel"
+
+       elif [[ $user="leonel" ]]; then
+
+        echo " leonel always wins"
+        else
+
+echo "You die"
+
 fi
